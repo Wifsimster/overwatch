af87abc04a26ce07d5b293e090e4d008ce8ed3d7
Wifsimster <wifsimster@gmail.com>
2017-02-21 08:52:29 +0100

    f

--- src/back/api/yeelight.js
+++ src/back/api/yeelight.js
@@ -4,21 +4,20 @@ const YeelightSearch = require('yeelight-wifi')
 module.exports = function (socket) {
 
     const yeelightSearch = new YeelightSearch()
-    
+
     socket.on('get.lights', (fn) => {
         yeelightSearch.on('found', () => {
 
             var lights = yeelightSearch.getYeelights()
+            var ids = []
 
-            lights.forEach((light) => {
+            for(var i = 0 ; i < lights.length; i++) {
                 console.log('-- Id :', light.getId())
                 console.log('-- Name :', light.getName())
                 console.log('-- Model :', light.getModel())
-
-                light.turnOn('smooth', 500)
-            })
-
-            fn(lights)
+                ids.push(lights[i].getId())
+            }
+            fn(ids)
         })
     })
 }
