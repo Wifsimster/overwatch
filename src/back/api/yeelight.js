fbc7f5f28d9512da783aa2443a4a49b46edea78b
Wifsimster <wifsimster@gmail.com>
2017-02-21 08:28:05 +0100

    f

--- src/back/api/yeelight.js
+++ src/back/api/yeelight.js
@@ -5,23 +5,23 @@ module.exports = function (socket) {
 
     const yeelightSearch = new YeelightSearch()
 
-    yeelightSearch.on('found', (fn) => {
+    socket.on('get.lights', (fn) => {
+        
+        console.log('Socket on get lights')
+        
+        yeelightSearch.on('found', (fn) => {
+            
+            var lights = yeelightSearch.getYeelights()
 
-        var lights = yeelightSearch.getYeelights()
-
-        socket.on('get.lights', (fn) => {
-            console.log('Socket on get lights')
             fn(lights)
+
+            lights.forEach((light) => {
+                console.log('-- Id :', light.getId())
+                console.log('-- Name :', light.getName())
+                console.log('-- Model :', light.getModel())
+
+                light.turnOn('smooth', 500)
+            })
         })
-
-        lights.forEach((light) => {
-            console.log('-- Id :', light.getId())
-            console.log('-- Name :', light.getName())
-            console.log('-- Model :', light.getModel())
-
-            light.turnOn('smooth', 500)
-        })
-
     })
-
 }
