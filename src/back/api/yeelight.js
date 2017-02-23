6278be9677bce45696705bd0d9c993d755c7a7ce
Wifsimster <wifsimster@gmail.com>
2017-02-23 08:49:41 +0100

    f

--- src/back/api/yeelight.js
+++ src/back/api/yeelight.js
@@ -3,27 +3,27 @@ const YeelightSearch = require('yeelight-wifi')
 
 module.exports = function (socket) {
 
-    const yeelightSearch = new YeelightSearch()
-    var lights = []
-
     try {
-    yeelightSearch.on('found', () => {
-        lights = yeelightSearch.getYeelights()
-        let object = []
-        for(var i = 0; i < lights.length ; i++) {
-            object.push({
-                id: lights[i].getId(),
-                model: lights[i].getModel(),
-                name: lights[i].getName()
-            })
-        }        
-        socket.broadcast.emit('found.lights', object)
-    })
+        const yeelightSearch = new YeelightSearch()
+        const lights = []
+        yeelightSearch.on('found', () => {
+            lights = yeelightSearch.getYeelights()
+            let object = []
+            for(var i = 0; i < lights.length ; i++) {
+                object.push({
+                    id: lights[i].getId(),
+                    model: lights[i].getModel(),
+                    name: lights[i].getName()
+                })
+            }        
+            socket.broadcast.emit('found.lights', object)
+        })
     } catch(err) {
         console.error('Error: ', err)
     }
 
     socket.on('get.light.values', (id, fn) => {
+        const yeelightSearch = new YeelightSearch()
         const light = yeelightSearch.getYeelightById(id)
         light.getValues(['power', 'bright']).then((values) => {          
             socket.emit('get.light.return', values)
@@ -33,6 +33,7 @@ module.exports = function (socket) {
     })
 
     socket.on('toggle.light', (id) => {
+        const yeelightSearch = new YeelightSearch()
         const light = yeelightSearch.getYeelightById(id)
         light.toggle().then((rst) => {
             socket.emit('toggle.light.return', rst)
@@ -42,6 +43,7 @@ module.exports = function (socket) {
     })
 
     socket.on('set.light.name', (options, fn) => {
+        const yeelightSearch = new YeelightSearch()
         const light = yeelightSearch.getYeelightById(options.id)
         light.setName(options.name).then((rst) => {
             socket.emit('set.light.name.return', rst)
