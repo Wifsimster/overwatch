f25465aeae6d25f1c5c4ea86d86994d17aac3574
Wifsimster <wifsimster@gmail.com>
2017-05-08 19:09:55 +0200

    Add cron

--- src/back/controllers/scenario.js
+++ src/back/controllers/scenario.js
@@ -1,3 +1,5 @@
 module.exports = (device, io) => {
-    require('../scenario/motion')(device, io)
+//    require('../scenario/motion')(device, io)
+    require('../scenario/morning')(device, io)
+    require('../scenario/night')(device, io)
 }
---
+++ src/back/scenario/night.js
@@ -0,0 +1,67 @@
+const Light = require('../class/light')
+const Device = require('../models/device')
+const Message = require('../models/message')
+const light = new Light()
+const moment = require('moment')
+
+const SLEEP_TIMER = 60000
+const LIVING_ROOM_LIGHT = "0x0000000003360d2c"
+const HALLWAY_LIGHT = "0x00000000033601d3"
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time))
+}
+
+module.exports = (device, io) => {
+    if(device && io) {
+        let date = new moment()
+        // Beetwen 01:00 - 07:00
+        if(date.hours() >= 01 && date.hours() <= 07) {
+            if(device.mac === '18:fe:34:d3:29:0e') {
+                if(device.message && device.message.data && device.message.data.state === '1') {
+
+                    // Get TSL2561 value
+                    Device.findAll({
+                        where: { mac: '18:fe:34:d7:f1:cd' },
+                    }).then((devices) => {
+                        let device = devices[0]
+                        Message.findAll({
+                            order: 'createdAt DESC',
+                            limit: 1,
+                            where: { deviceId: device.id }
+                        }).then((messages) => {                        
+                            let data = JSON.parse(messages[0].data)
+                            let luminosity = data.luminosity                        
+                            if(luminosity <= 15) {
+                                io.emit('notify', {
+                                    message: 'Présence détectée. <br>Lumières allumées pour 30s !',
+                                    type: 'success',
+                                    time: 15,
+                                    data: device
+                                })
+
+                                light.setBrightness({brightness: 05, id: LIVING_ROOM_LIGHT})
+                                light.setBrightness({brightness: 05, id: HALLWAY_LIGHT})
+
+                                light.turnOn(LIVING_ROOM_LIGHT)
+                                light.turnOn(HALLWAY_LIGHT)
+
+                                sleep(SLEEP_TIMER).then(() => {
+                                    light.turnOff(LIVING_ROOM_LIGHT)
+                                    light.turnOff(HALLWAY_LIGHT)
+                                })
+                            }
+                        })
+                    }).catch((err) => {
+                        console.error(err)
+                        io.emit('notify', {
+                            message: err,
+                            type: 'error',
+                            time: 5,
+                        })
+                    })
+                }
+            }
+        }
+    }
+}
